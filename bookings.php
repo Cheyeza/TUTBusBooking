9a3c94ddef16d4689380c895de2936244198e5f7
cheyeza <cheyezamlondo55@gmail.com>
2021-11-26 21:08:33 +0200

    removed a comment

--- bookings.php
+++ bookings.php
@@ -1,63 +1,116 @@
 <?php
 
-if (session_status()!==PHP_SESSION_ACTIVE)session_start();
+    if (session_status()!==PHP_SESSION_ACTIVE) session_start();
+    $_SESSION['booking_error'] = null;
+    if (!isset($_SESSION['studNo']))
+    {
+        header("Location: index.php");
+    }
+    include 'connect.php';
 
-if (!isset($_SESSION['studNo']))
-{
-    header("Location: index.php");
-}
-include "connect.php";
-//require "home.php";
+    $query="select * from student where studNo='".$_SESSION['studNo']."'";
+    $result=mysqli_query($conn,$query);
+    $row=mysqli_fetch_array($result);
+    $rowCount = 0;
 ?>
-<!DOCTYPE html>
+
 <html>
-
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>New Booking</title>
-  
-</head>
-
-<body>
-    <!-- This is the navigation-->
-<?php include 'navigation.html'; ?>
-<div class="back">
-<div class="div-center">
-
-
-<div class="container align-items-center" style="width: 50%">
-<h2 class="text-center shadow-sm form-heading" style="font-family: Asap, sans-serif;font-weight: normal;color: rgb(240,62,51);">Booking Form &nbsp;</h2>
-<h1 style="font-size: 16px;font-family: Asap, sans-serif;color: rgb(240,62,51);font-style: italic;">Select Your Trip</h1>
+<meta charset="UTF-8">
+<meta name="viewport"
+content="width=device.width,initial-scale=1">
+<link rel="stylesheet"
+href="https://www.w3schools.com/w3css/3/w3.css">
+    <head>
+        <title>Bookings</title>
+        
+            </head>
                
-<!-- Dropdown list with dummy data -->
-<form action = "schedules.php" method = "POST">
-    
-    <select name="location"  class="form-control" id="location">
+    <body>
+
+    <?php include 'navigation.html'; ?>
+
+
+
+
+
+
+
+        <div class="container">
+        <h2 style="color: rgb(240,62,51);"> Hi, <?php echo $row['name'];?>  </h2>
+    <!--slide show-->
+
+        
+
         <?php
-            
-            $query="select * from campus  where (select count(*) from schedule group by campus_From having campus_From = campusId) > 0 and  (select count(*) from schedule where time >NOW()) > 0 order by campusName;";
-
+            $query="select * from view_bookings where studNo = '".$_SESSION['studNo']."'  order by time";
             $result=mysqli_query($conn,$query) or die(mysqli_error($conn));
+            $rowCount = mysqli_num_rows($result);
 
-            foreach($result as $row){
-                echo '<option value="'.$row['campusId'] .'">'.$row['campusName'].'</option>';
-
-            }
-
-        ?>
             
-</select>
-<br>
-        <button type="submit" class="btn btn-primary">Next</button>
-        </div></div></div>
-    
-    
-        </form >
+            
+            if($rowCount > 0){
+        ?>
+        <table class="table table-striped thead-dark table-condensed">
+            <thead class="thead ">
+                <tr>
+                    <th>Departing From </th>
+                    <th>Traveling To </th>
+                    <th>Date</td>
+                    <th>Time</td>
+                    <th>Cancel</th>
+                </tr>
+            </thead>
+            <tbody>
 
-  
-         </body>
+            <!-- dummy bookings-->
+            <?php
+                   
+                    foreach($result as $row){
+            ?>
+                <tr>
 
+               
+            
+
+
+            
+                    <td><?php echo $row['departure']; ?> </td>
+                    <td><?php echo $row['destination'] ?> </td>
+                    <td><?php echo date_create($row['time'])->format('Y-m-d') ?> </td>
+                    <td><?php echo date_create($row['time'])->format('H:i') ?> </td>
+                  
+                    <td>
+                        <form method="POST" action="process_cancellation.php"> 
+                            <input type="hidden" value="<?php echo $row['bookingId'];?>" name="bookingId"/>
+                            <input class="btn btn-danger" type="submit" value="Cancel"/>
+                        </form>
+                    </td>
+                </tr>
+                <?php  }
+
+                ?>
+                
+            <tbody>
+        </table>
+        <?php
+            }
+            else{
+
+
+                ?>
+
+<div class="alert alert-danger">
+    <span colspan="14">You have no booked trips</span>
+            </div>
+
+<?php
+            }
+            ?>
+        
+        </div>
+        <div>
+
+</div>
+<?php if($rowCount > 5) include 'footer_bottom.html'; else include 'footer_fixed.html' ;?>
+    </body>
 </html>
- 
